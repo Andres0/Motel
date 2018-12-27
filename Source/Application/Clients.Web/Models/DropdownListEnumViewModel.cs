4df8b11a7e1e3c959133f84621750671d902332f
Pedro Luis del Castillo <38741752+vicio19@users.noreply.github.com>
2018-12-26 23:46:34 -0400

    Signed-off-by: Pedro Luis del Castillo <38741752+vicio19@users.noreply.github.com>

---
+++ Source/Application/Clients.Web/Models/DropdownListEnumViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DS.Motel.Clients.Web.Models
+{
+    public class DropdownListEnumViewModel
+    {
+        public int  Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
