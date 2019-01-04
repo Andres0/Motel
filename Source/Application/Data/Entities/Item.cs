9917e3a3d8fabde58b1b311ba14af66a13d4779b
Andres0 <dreco71@hotmail.com>
2019-01-03 23:04:59 -0400

    Signed-off-by: Andres0 <dreco71@hotmail.com>

---
+++ Source/Application/Data/Entities/Item.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.Motel.Data.Entities
+{
+    public class Item
+    {
+        #region Fields & Properties
+
+        public Guid ItemId { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Guid CategoriaId { get; set; }
+        public virtual ItemCategoria Categoria { get; set; }
+
+        public decimal Saldo_Inicial { get; set; }
+        public decimal Cantidad_Stock { get; set; }
+        public decimal Stock_Minimo { get; set; }
+
+
+        #endregion
+
+
+
+
+
+
+        #region Constructors
+
+        public Item()
+        {
+            ItemId = Guid.NewGuid();
+        }
+
+        #endregion
+    }
+}
