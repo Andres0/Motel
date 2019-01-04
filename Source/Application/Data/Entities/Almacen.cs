df07799358374fbe76a55bfe3fe365d161070861
Andres0 <dreco71@hotmail.com>
2019-01-03 22:47:53 -0400

    Signed-off-by: Andres0 <dreco71@hotmail.com>

---
+++ Source/Application/Data/Entities/Almacen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.Motel.Data.Entities
+{
+    public class Almacen
+    {
+        #region Fields & Properties
+
+        public Guid AlmacenId { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+
+        #endregion
+
+
+
+        #region MyRegion
+
+        public Almacen()
+        {
+            AlmacenId = new Guid();
+        }
+
+        #endregion
+    }
+}
+}
---
+++ Source/Application/Data/Entities/ItemCategoria.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS.Motel.Data.Entities
+{
+    public class ItemCategoria
+    {
+        #region Fields & Properties
+
+        public Guid ItemCategoriaId { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Guid? PadreId { get; set; }
+        public virtual ItemCategoria Padre { get; set; }
+
+        #endregion
+
+
+
+        #region MyRegion
+
+        public ItemCategoria()
+        {
+            ItemCategoriaId = new Guid();
+        }
+
+        #endregion
+    }
+}
